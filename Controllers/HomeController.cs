409ebb6a32f63901ce0cd3872c4cc7eb396ef146
Naveen2205 <naveenram2205@gmail.com>
2021-09-22 16:03:01 +0530

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestApi.Services;
+
+
+
+namespace RestApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHomeServicer _homeServicer;
+
+        public HomeController(
+               IHomeServicer homeServicer
+            )
+        {
+            _homeServicer = homeServicer;
+        }
+        [HttpGet]
+        public string Index()
+        {
+           
+            return _homeServicer.DisplayText();
+        }
+
+        [HttpGet]
+        public string GetById()
+        {
+            return "Get By Id";
+        } 
+
+        [HttpPost]
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        [HttpPut]
+        public string UpdateById()
+        {
+            return "Update by Id";
+        }
+
+        [HttpDelete]
+        public string DeleteById()
+        {
+            return "Delete By Id";
+        }
+    }
+}
---
+++ Services/HomeServicer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NugetTestLib;
+using NugetLocalLib;
+using Newtonsoft.Json;
+
+namespace RestApi.Services
+{
+    public interface IHomeServicer
+    {
+        public string DisplayText();
+    }
+    public class HomeServicer : IHomeServicer
+    {
+        public string DisplayText()
+        {
+            return Class1.NugetPrinter("nuget printer test");
+        }
+    }
+}
